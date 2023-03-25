a71176d48ead6e5f48bf5e15012e4e014a6eaad3
Andrey Shakurov <amshakurov@gmail.com>
2023-03-25 19:10:14 +0700

    create table

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import Example from "./components/Table.jsx";
+
+const App = (props) => {
+    return (
+        <div className='app-wrapper'>
+            <Example/>
+        </div>
+    )
+}
+
+export default App
